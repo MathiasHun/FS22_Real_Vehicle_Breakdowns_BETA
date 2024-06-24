3744b59fd7659bdb36dc97a13a8d57752f7edeb5
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2024-06-24 21:44:18 +0200

    Update RVBGeneralSet_Event.lua - 0.8.7.0

--- scripts/events/RVBGeneralSet_Event.lua
+++ scripts/events/RVBGeneralSet_Event.lua
@@ -9,27 +9,33 @@ function RVBGeneralSet_Event.emptyNew()
     return self
 end
 
-function RVBGeneralSet_Event.new(alertmessage, rvbDifficulty)
+function RVBGeneralSet_Event.new(alertmessage, rvbDifficulty, basicrepairtrigger, cp_notice)
     local self = RVBGeneralSet_Event.emptyNew()
 	self.alertmessage = alertmessage
 	self.rvbDifficulty = rvbDifficulty
+	self.basicrepairtrigger = basicrepairtrigger
+	self.cp_notice = cp_notice
     return self
 end
 
 function RVBGeneralSet_Event:readStream(streamId, connection)
 	self.alertmessage = streamReadBool(streamId)
 	self.rvbDifficulty = streamReadInt32(streamId)
+	self.basicrepairtrigger = streamReadBool(streamId)
+	self.cp_notice = streamReadBool(streamId)
     self:run(connection)
 end
 
 function RVBGeneralSet_Event:writeStream(streamId, connection)
 	streamWriteBool(streamId, self.alertmessage)
 	streamWriteInt32(streamId, self.rvbDifficulty)
+	streamWriteBool(streamId, self.basicrepairtrigger)
+	streamWriteBool(streamId, self.cp_notice)
 end
 
 function RVBGeneralSet_Event:run(connection)
 	if not connection:getIsServer() then
-		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.rvbDifficulty), nil, connection, self)
+		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.rvbDifficulty, self.basicrepairtrigger, self.cp_notice), nil, connection, self)
 	end
-	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.rvbDifficulty)
+	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.rvbDifficulty, self.basicrepairtrigger, self.cp_notice)
 end

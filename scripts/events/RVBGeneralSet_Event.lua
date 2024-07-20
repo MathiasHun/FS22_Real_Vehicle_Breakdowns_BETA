6bf0a2189fe92fa092aefdee401d117cad37a345
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2024-07-20 18:34:18 +0200

    Update RVBGeneralSet_Event.lua - 0.8.8.0

--- scripts/events/RVBGeneralSet_Event.lua
+++ scripts/events/RVBGeneralSet_Event.lua
@@ -9,10 +9,10 @@ function RVBGeneralSet_Event.emptyNew()
     return self
 end
 
-function RVBGeneralSet_Event.new(alertmessage, rvbDifficulty, basicrepairtrigger, cp_notice)
+function RVBGeneralSet_Event.new(alertmessage, difficulty, basicrepairtrigger, cp_notice)
     local self = RVBGeneralSet_Event.emptyNew()
 	self.alertmessage = alertmessage
-	self.rvbDifficulty = rvbDifficulty
+	self.difficulty = difficulty
 	self.basicrepairtrigger = basicrepairtrigger
 	self.cp_notice = cp_notice
     return self
@@ -20,7 +20,7 @@ end
 
 function RVBGeneralSet_Event:readStream(streamId, connection)
 	self.alertmessage = streamReadBool(streamId)
-	self.rvbDifficulty = streamReadInt32(streamId)
+	self.difficulty = streamReadInt32(streamId)
 	self.basicrepairtrigger = streamReadBool(streamId)
 	self.cp_notice = streamReadBool(streamId)
     self:run(connection)
@@ -28,14 +28,14 @@ end
 
 function RVBGeneralSet_Event:writeStream(streamId, connection)
 	streamWriteBool(streamId, self.alertmessage)
-	streamWriteInt32(streamId, self.rvbDifficulty)
+	streamWriteInt32(streamId, self.difficulty)
 	streamWriteBool(streamId, self.basicrepairtrigger)
 	streamWriteBool(streamId, self.cp_notice)
 end
 
 function RVBGeneralSet_Event:run(connection)
 	if not connection:getIsServer() then
-		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.rvbDifficulty, self.basicrepairtrigger, self.cp_notice), nil, connection, self)
+		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.cp_notice), nil, connection, self)
 	end
-	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.rvbDifficulty, self.basicrepairtrigger, self.cp_notice)
+	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.cp_notice)
 end

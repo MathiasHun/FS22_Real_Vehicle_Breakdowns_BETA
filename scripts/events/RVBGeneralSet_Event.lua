c8880c36fd57cdbdda497cd1d757ff09ef7b0801
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2024-11-06 15:40:27 +0100

    0.9.0.1

--- scripts/events/RVBGeneralSet_Event.lua
+++ scripts/events/RVBGeneralSet_Event.lua
@@ -9,11 +9,12 @@ function RVBGeneralSet_Event.emptyNew()
     return self
 end
 
-function RVBGeneralSet_Event.new(alertmessage, difficulty, basicrepairtrigger, cp_notice)
+function RVBGeneralSet_Event.new(alertmessage, difficulty, basicrepairtrigger, debugdisplay, cp_notice)
     local self = RVBGeneralSet_Event.emptyNew()
 	self.alertmessage = alertmessage
 	self.difficulty = difficulty
 	self.basicrepairtrigger = basicrepairtrigger
+	self.debugdisplay = debugdisplay
 	self.cp_notice = cp_notice
     return self
 end
@@ -22,6 +23,7 @@ function RVBGeneralSet_Event:readStream(streamId, connection)
 	self.alertmessage = streamReadBool(streamId)
 	self.difficulty = streamReadInt32(streamId)
 	self.basicrepairtrigger = streamReadBool(streamId)
+	self.debugdisplay = streamReadBool(streamId)
 	self.cp_notice = streamReadBool(streamId)
     self:run(connection)
 end
@@ -30,12 +32,13 @@ function RVBGeneralSet_Event:writeStream(streamId, connection)
 	streamWriteBool(streamId, self.alertmessage)
 	streamWriteInt32(streamId, self.difficulty)
 	streamWriteBool(streamId, self.basicrepairtrigger)
+	streamWriteBool(streamId, self.debugdisplay)
 	streamWriteBool(streamId, self.cp_notice)
 end
 
 function RVBGeneralSet_Event:run(connection)
 	if not connection:getIsServer() then
-		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.cp_notice), nil, connection, self)
+		g_server:broadcastEvent(RVBGeneralSet_Event.new(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.debugdisplay, self.cp_notice), nil, connection, self)
 	end
-	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.cp_notice)
+	g_currentMission.vehicleBreakdowns:setCustomGeneralSet(self.alertmessage, self.difficulty, self.basicrepairtrigger, self.debugdisplay, self.cp_notice)
 end
